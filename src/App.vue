65aa57016758b010982f64a373d1cd2ef3810756
Unknown <Justin-Brown@web.de>
2019-04-17 00:06:07 +0200

    Switched to vuetify

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-app dark>
+    <v-toolbar app>
+      <v-toolbar-title>
+        <v-img
+          :src="require('@/assets/DDD_Logo.png')"
+          class="mr-2"
+          contain
+          width="150"
+        ></v-img>
+      </v-toolbar-title>
+      <!-- <v-toolbar-title class="headline text-uppercase">
+        <span>Densha </span>
+        <span class="font-weight-light">De D</span>
+      </v-toolbar-title> -->
+      <template>
+        <v-toolbar-items>
+          <v-btn flat class="headline font-weight-light text-uppercase" to="/">Home</v-btn>
+        </v-toolbar-items>
+        <!-- Games -->
+        <v-menu>
+          <template v-slot:activator="{ on }">
+            <v-toolbar-items v-on="on">
+              <v-btn flat class="headline font-weight-light text-uppercase">
+                Games
+                <v-icon right dark>fas fa-caret-down</v-icon>
+              </v-btn>
+            </v-toolbar-items>
+          </template>
+
+          <v-list>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Lightning Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Burning Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Climax Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Rising Stage
+              </v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+        <!-- Get The Game -->
+        <v-menu>
+          <template v-slot:activator="{ on }">
+            <v-toolbar-items v-on="on">
+              <v-btn flat class="headline font-weight-light text-uppercase">
+                Get The Game
+                <v-icon right dark>fas fa-caret-down</v-icon>
+              </v-btn>
+            </v-toolbar-items>
+          </template>
+
+          <v-list>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Lightning Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Burning Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Climax Stage
+              </v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile to="/about">
+              <v-list-tile-title class="font-weight-light text-uppercase">
+                Rising Stage
+              </v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
+      </template>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>fas fa-external-link-alt</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>

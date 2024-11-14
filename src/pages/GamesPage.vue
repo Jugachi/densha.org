c510fc15957a5bebe2e6cfabb7c127059c98475e
Jugachi <Justin-Brown@web.de>
2024-11-14 17:51:51 +0100

    Added tabs for the GamesPage.vue Updated all packages

--- src/pages/GamesPage.vue
+++ src/pages/GamesPage.vue
@@ -1,29 +1,51 @@
 <template>
-  <div>
-    <h1>{{ game.name }}</h1>
-    <p>{{ game.description }}</p>
+  <div style="width: 66%; margin: 0 auto;">
+    <div class="d-flex justify-center">
+      <v-tabs v-model="activeTab" grow color="primary">
+        <v-tab value="game">Game</v-tab>
+        <v-tab value="characters">Characters</v-tab>
+        <v-tab value="trains">Trains</v-tab>
+      </v-tabs>
 
-	<div v-if="game.characters">
-      <h2>{{ game.characters.name }}</h2>
-
-      <!-- Loop through each description and render HTML -->
-      <div
-        v-for="(desc, index) in game.characters.description"
-        :key="index"
-        v-html="desc"
-      ></div>
     </div>
 
-    <div v-if="game.trains">
-      <h2>{{ game.trains.name }}</h2>
+    <v-tabs-window v-model="activeTab">
+      <v-tabs-window-item value="game">
+        <div class="text-center">
+          <div class="d-flex align-center justify-center">
+            <img :src="gameImagePath" class="gameCover"/>
+            <div>
+              <h1>{{ game.name }}</h1>
+              <p>{{ game.description }}</p>
 
-      <!-- Loop through each description and render HTML -->
-      <div
-        v-for="(desc, index) in game.trains.description"
-        :key="index"
-        v-html="desc"
-      ></div>
-    </div>
+            </div>
+          </div>
+          
+        </div>
+      </v-tabs-window-item>
+
+      <v-tabs-window-item value="characters">
+        <div v-if="game.characters" class="text-center">
+          <h2>{{ game.characters.name }}</h2>
+          <div
+            v-for="(desc, index) in game.characters.description"
+            :key="index"
+            v-html="desc"
+          ></div>
+        </div>
+      </v-tabs-window-item>
+
+      <v-tabs-window-item value="trains">
+        <div v-if="game.trains" class="text-center">
+          <h2>{{ game.trains.name }}</h2>
+          <div
+            v-for="(desc, index) in game.trains.description"
+            :key="index"
+            v-html="desc"
+          ></div>
+        </div>
+      </v-tabs-window-item>
+    </v-tabs-window>
   </div>
 </template>
 
@@ -31,10 +53,11 @@
 export default {
   data() {
     return {
+      activeTab: 0,
       game: {
         name: "",
         description: "",
-		characters: {
+        characters: {
           name: "",
           description: [],
         },
@@ -48,6 +71,11 @@ export default {
   computed: {
     slug() {
       return this.$route.params.slug;
+    },
+    gameImagePath() {
+      // Dynamically construct the image path based on game name
+      console.log(this.$route.params.slug);
+      return new URL(`/src/assets/covers/${this.$route.params.slug}_cover.jpg`, import.meta.url).href;
     },
   },
   watch: {
@@ -81,3 +109,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.gameCover {
+  width: 256px; /* Set the width to fit nicely with the title */
+  height: auto;
+  margin: 10px; /* Add some space between the title and the image */
+}
+</style>

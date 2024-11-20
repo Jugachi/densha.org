88796047988f5473b1a767a24ae91ea04ea355bd
exe1c <158598973+xolvz@users.noreply.github.com>
2024-11-20 12:29:29 +0100

    added buy buttons to the games page and added the correct colors to the nav bar

---
+++ src/components/WarningDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog v-model="dialog" max-width="500">
+    <v-card>
+      <v-card-title class="headline">External Link Confirmation</v-card-title>
+      <v-card-text>
+        You are about to visit an external website. You might need a VPN to access it. Do you want to proceed?
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="confirm">Yes</v-btn>
+        <v-btn color="red darken-1" text @click="dialog = false">No</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    openDialog() {
+      this.dialog = true;
+    },
+    confirm() {
+      window.open(this.link, "_blank");
+      this.dialog = false;
+    }
+  }
+};
+</script>
--- src/pages/GamesPage.vue
+++ src/pages/GamesPage.vue
@@ -1,104 +1,93 @@
 <template>
   <div style="width: 66%; margin: 1rem auto;">
 
-
-    <v-tabs v-model="activeTab" grow color="primary" style="margin-bottom: 1rem;">
+    <v-tabs v-model="activeTab" grow :color="thisgame.color" style="margin-bottom: 1rem;">
       <v-tab value="game">Game</v-tab>
       <v-tab value="characters">Characters</v-tab>
       <v-tab value="trains">Trains</v-tab>
-      <v-tab value="buy" @click="confirmNavigation()">Buy</v-tab>
+      <div class="text-center">
+          <v-btn :color="thisgame.color" style="height: 100%;" @click="buyGameDialog" variant="text">Buy the Game</v-btn>
+      </div>
+      <div class="text-center">
+          <v-btn :color="thisgame.color" style="height: 100%;" @click="buyOstDialog" variant="text">Buy the Ost</v-btn>
+      </div>
     </v-tabs>
 
     <v-tabs-window v-model="activeTab">
-
-      <!-- Game tab -->
       <v-tabs-window-item value="game">
         <div class="text-center">
-          <div class="text-center">
-            <div class="d-flex justify-center">
-
-              <!-- Game cover -->
-              <img :src="gameImagePath" class="gameCover"/>
-
-              <!-- Game data -->
-              <div>
-                <h1>{{ game.name }}</h1>
-                <p>{{ game.description }}</p>
-              </div>
+          <div class="d-flex justify-center">
+            <img :src="gameImagePath" class="gameCover"/>
+            <div>
+              <h1>{{ game.name }}</h1>
+              <p>{{ game.description }}</p>
             </div>
           </div>
         </div>
       </v-tabs-window-item>
 
-      <!-- Characters tab -->
       <v-tabs-window-item value="characters">
         <div v-if="game.characters.length" class="text-center">
           <div v-for="(character, index) in game.characters" :key="index" class="d-flex justify-center">
-            <!-- Character img -->
             <img :src="getCharacterImagePath(character.name)" v-if="(index%2)===0" class="img"/>
-            <!-- Character data -->
             <div :class="[{'align-right': (index%2)!==0}, {'align-left': (index%2)===0}]">
               <h2>{{ character.alias }}</h2>
               <div v-for="(desc, index) in character.description" :key="index" v-html="desc"></div>
             </div>
-            
-            <img :src="getCharacterImagePath(character.name)" v-if="(index%2)!==0" class="img" />
+            <img :src="getCharacterImagePath(character.name)" v-if="(index%2)!==0" class="img"/>
           </div>
         </div>
       </v-tabs-window-item>
 
-      <!-- Trains tab -->
       <v-tabs-window-item value="trains">
         <div v-if="game.trains.length" class="text-center">
           <div v-for="(train, index) in game.trains" :key="index" class="d-flex justify-center">
-            <!-- Train img on the right -->
-            <img :src="getTrainImagePath(train.name)" v-if="(index%2)===0" class="img" />
-            <!-- Train data -->
+            <img :src="getTrainImagePath(train.name)" v-if="(index%2)===0" class="img"/>
             <div :class="[{'align-right': (index%2)!==0}, {'align-left': (index%2)===0}]">
               <h2 style="margin: 1rem 1rem 1rem 1rem;">{{ train.alias }}</h2>
-              
-
               <div v-for="(desc, index) in train.description" :key="index" v-html="desc" style="margin: 1rem 1rem 1rem 1rem;"></div>
             </div>
-            
-
-            <!-- Train image on the left -->
-            <img :src="getTrainImagePath(train.name)" v-if="(index%2)!==0" class="img" />
+            <img :src="getTrainImagePath(train.name)" v-if="(index%2)!==0" class="img"/>
           </div>
         </div>
+      </v-tabs-window-item>
 
-      </v-tabs-window-item>
-      <v-tabs-window-item value="buy">
-        <div class="text-center">
-          <v-btn color="primary" @click="buyGame">Buy the Game</v-btn>
-        </div>
-      </v-tabs-window-item>
     </v-tabs-window>
-    
+
+
+    <v-snackbar v-model="error" location="center" :timeout="2000" color="red" variant="outlined">
+      {{ errorMessage }}
+      
+    </v-snackbar>
+
+
+    <WarningDialog ref="warningDialog" :link="buyGameLink" />
   </div>
 </template>
 
 <script>
+import WarningDialog from '@/components/WarningDialog.vue';
+import variables from '@/locales/variables.json';
+
 export default {
   data() {
     return {
-      activeTab: 0,
+      activeTab: 'game',
       game: {
         name: "",
         description: "",
         characters: [],
         trains: [],
       },
+      buyGameLink: "",
+    
 
-      buyGames: [
-        { name: "Lightning Stage", link: "https://www.dlsite.com/home/dlaf/=/t/s/link/work/aid/DenshaOrg/locale/en_US/id/RJ217271.html/?locale=en_US" },
-        { name: "Burning Stage", link: "https://www.dlsite.com/home/dlaf/=/t/s/link/work/aid/DenshaOrg/locale/en_US/id/RJ227913.html/?locale=en_US" },
-        { name: "Climax Stage", link: "https://www.dlsite.com/home/dlaf/=/t/s/link/work/aid/DenshaOrg/locale/en_US/id/RJ242866.html/?locale=en_US" },
-        { name: "Rising Stage", link: "https://www.dlsite.com/home/dlaf/=/t/s/link/work/aid/DenshaOrg/locale/en_US/id/RJ242921.html/?locale=en_US" },
-        { name: "Shining Stage", link: "https://www.dlsite.com/home/dlaf/=/t/s/link/work/aid/DenshaOrg/locale/en_US/id/RJ293227.html/?locale=en_US" },
-      ],
-      
+      error: false,
+      errorMessage: "",
     };
+  },
+  components: {
+    WarningDialog
   },
   computed: {
     slug() {
@@ -107,18 +96,23 @@ export default {
     gameImagePath() {
       return new URL(`/src/assets/covers/${this.$route.params.slug}_cover.jpg`, import.meta.url).href;
     },
+    thisgame() {
+      return variables.games.find(game => game.name.toLocaleLowerCase().includes(this.slug)) || {};
+    }
   },
   methods: {
-    getNumberFromString(str) {
-      const match = str.match(/\d+$/);
-      return match ? match[0] : '';
+
+    showError(message) {
+      this.error = true;
+      this.errorMessage = message;
     },
+
     getCharacterImagePath(name) {
-      const formattedName = name.replace(/\s/g, "_").toLowerCase();
+      const formattedName = name.replace(/\s/g, "_").toLowerCase(); // replace every space with an underscore and convert to lowercase
       return new URL(`/src/assets/characters/${this.$route.params.slug}/${formattedName}.png`, import.meta.url).href;
     },
     getTrainImagePath(name) {
-      const formattedName = this.getNumberFromString(name);
+      const formattedName = name.replace(/\D/g, ""); // replace everything that is not a digit with an empty string
       return new URL(`/src/assets/trains/${this.$route.params.slug}/icon_${formattedName}.png`, import.meta.url).href;
     },
     fetchGameData(slug) {
@@ -130,12 +124,24 @@ export default {
         this.game.trains = gameData.trains || [];
       }
     },
-    buyGame() {
-      const game = this.buyGames.find(game => game.name.toLowerCase().includes(this.slug));
-      if (game) {
-        window.open(game.link, "_blank");
+    buyGameDialog() {
+      if (this.thisgame.gameLink) {
+        this.buyGameLink = this.thisgame.gameLink;
+        this.$refs.warningDialog.openDialog();
       } else {
-        alert('No purchase link available for this game.');
+        
+
+        this.showError('No purchase link available for this game.');
+      }
+    },
+    buyOstDialog() {
+      if (this.thisgame.ostLink) {
+        this.buyGameLink = this.thisgame.ostLink;
+        this.$refs.warningDialog.openDialog();
+      } else {
+        
+
+        this.showError('No purchase link available for this game.');
       }
     },
   },
@@ -145,27 +151,27 @@ export default {
     },
     "$i18n.locale"() {
       this.fetchGameData(this.slug);
-    },
+    }
   },
   mounted() {
     this.fetchGameData(this.slug);
-  },
+  }
 };
 </script>
 
 <style scoped>
 .gameCover {
-  width: 256px; /* Set the width to fit nicely with the title */
+  width: 256px;
   height: auto;
-  margin: 10px; /* Add some space between the title and the image */
+  margin: 10px;
 }
 
 .align-right {
   text-align: right;
   margin-right: 1rem;
 }
-.align-left
-{
+
+.align-left {
   text-align: left;
   margin-left: 1rem;
 }
@@ -175,5 +181,4 @@ export default {
   height: fit-content;
   width: fit-content;
 }
-
 </style>
